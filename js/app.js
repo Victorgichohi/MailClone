24bd7948af32f33cc6a9f2db814af887ac1c3546
Victorgichohi <victorgichohi48@gmail.com>
2016-01-23 13:49:46 +0300

    connected the factories and directives

--- js/app.js
+++ js/app.js
@@ -5,14 +5,12 @@ angular.module('EmailApp', [
     'ngRoute'
 ]).config(function ( $routeProvider ) {
     'use strict';
-    // configure urls
     $routeProvider
-    // inbox route
         .when('/inbox', {
             templateUrl: 'views/inbox.html',
-            controller: 'InboxCtrl', // map js to html scope
+            controller: 'InboxCtrl'
         })
-        .otherwise({ // default
+        .otherwise({
             redirectTo: '/inbox'
         });
 });
