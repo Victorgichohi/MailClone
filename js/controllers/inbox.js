8aabd71d4a1193b4e04d44b08ea969f2b06b69fb
Victorgichohi <victorgichohi48@gmail.com>
2016-01-23 10:48:33 +0300

    added the controllers and views for the inbox view

---
+++ js/controllers/inbox.js
@@ -0,0 +1,12 @@
+/**
+ * Created by victor on 1/23/16.
+ */
+/**
+ * Controller: InboxCtrl
+ */
+angular.module('EmailApp')
+    .controller('InboxCtrl',
+        function InboxCtrl ( ) {
+            'use strict';
+            console.log('Inbox Loaded!')
+        });
